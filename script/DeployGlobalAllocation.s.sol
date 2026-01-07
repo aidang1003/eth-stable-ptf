14d0edb8aa35b45a6abd33627adafd381b08dec0
Aidan Gorny <aidang1003@gmail.com>
2026-01-06 20:55:51 -0700

    GlobalAllocation Deploy script complete

--- script/DeployGlobalAllocation.s.sol
+++ script/DeployGlobalAllocation.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {GlobalAllocation} from "src/GlobalAllocation.sol";
+import {USDC_ADDRESS} from "src/Constants.sol";
 
 contract DeployGlobalAllocation is Script {
 
@@ -12,10 +13,10 @@ contract DeployGlobalAllocation is Script {
 
     function deployContract() public returns (Allocation) {
         vm.startBroadcast();
-        address token = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48); //USDC mainnet address
-        Allocation allocation = new Allocation(token, 50); //50% ETH to USDC allocation
+        address token = USDC_ADDRESS;
+        GlobalAllocation globalAllocation = new GlobalAllocation(token, 50); //50% ETH to USDC allocation
         vm.stopBroadcast();
 
-        return (allocation);
+        return (globalAllocation);
     }
 }
