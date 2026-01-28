040319114c674faae373156766202189efab8d3d
0xfaced <aidang1003@gmail.com>
2026-01-27 18:59:39 -0700

    Implement helper config, delete allocation contracts

--- script/DeployGlobalAllocation.s.sol
+++ script/DeployGlobalAllocation.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {GlobalAllocation} from "src/GlobalAllocation.sol";
-import {WETH_ADDRESS, USDC_ADDRESS, UNISWAP_V2_ROUTER02} from "src/Constants.sol";
+import {HelperConfig} from "HelperConfig.s.sol";
 
 contract DeployGlobalAllocation is Script {
     function run() public {
@@ -11,20 +11,19 @@ contract DeployGlobalAllocation is Script {
     }
 
     function deployContract(address _deployer) public returns (GlobalAllocation globalAllocation) {
-        if (block.chainid == 1) {
-            vm.startBroadcast(_deployer);
+        helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
-            globalAllocation = new GlobalAllocation({
-                _token1: WETH_ADDRESS,
-                _token2: USDC_ADDRESS,
-                _uniswapRouter: UNISWAP_V2_ROUTER02,
-                _desiredEthToTokenAllocationPercentage: 500000, //50% ETH to USDC allocation
-                _rebalancePercentage: 40000
-            }); //4% rebalance percentage
+        vm.startBroadcast(_deployer);
 
-            vm.stopBroadcast();
-        } else {
-            revert("Unsupported chain");
-        }
+        globalAllocation = new GlobalAllocation({
+            _token1: config.token1,
+            _token2: config.token2,
+            _uniswapRouter: config.uniswapRouter,
+            _desiredEthToTokenAllocationPercentage: config.desiredEthallocationPercentage,
+            _rebalancePercentage: config.rebalancePercentage
+        });
+
+        vm.stopBroadcast();
     }
 }
